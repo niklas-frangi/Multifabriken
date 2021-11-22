00bd489590f8cae3080083f7216216d630a915ab
Niklas Frangi <55104050+niklas-frangi@users.noreply.github.com>
2021-11-22 15:47:13 +0100

    Update WelcomeText.cs

--- WelcomeText.cs
+++ WelcomeText.cs
@@ -9,7 +9,7 @@ namespace multifabriken_niklas_frangi
             Console.Clear();
             Console.WriteLine("Introduktion:");
             Console.WriteLine();
-            Console.WriteLine("Här kan du beställa olika produkter från vår frabrik.");
+            Console.WriteLine("Här kan du beställa olika produkter från vår fabrik.");
             Console.WriteLine("Välj alternativ genom att skriva en siffra och tryck sedan på enterknappen.");
             Console.WriteLine("Du kan inte ta bort produkter efter att du beställt dem, så var försiktig!");
             Console.WriteLine();
